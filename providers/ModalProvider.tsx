f92280468c895f0fbed382c81c61c70b3c24cf43
ross-brown <rbrown_98@yahoo.com>
2023-12-29 16:57:22 -0600

    create modal component and provider

---
+++ providers/ModalProvider.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import Modal from "@/components/Modal";
+
+
+function ModalProvider() {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <>
+      <Modal
+        title="Test Modal"
+        description="Test Description"
+        isOpen
+        onChange={() => { }}
+      >
+        Test Children
+      </Modal>
+    </>
+  );
+}
+
+export default ModalProvider;
